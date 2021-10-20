dff898c8389db94174f7bcb5dd35c37cf2bbc826
weiztech <weiztech@gmail.com>
2021-10-20 16:24:50 +0700

    re-import change on lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 //! Rust client for Star Wars API
-
 pub mod common;
 pub mod objects;
 mod requests;
 
-pub use crate::objects::{Film, People, Planet, Species, Starship, Vehicle};
-pub use crate::requests::RequestHandler;
+pub use objects::{Film, People, Planet, Species, Starship, Vehicle};
+pub use requests::RequestHandler;
